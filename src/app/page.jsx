853ab5ac147f1c100c8635f0baf292885c132224
kasurbantal <kasurbantal880@gmail.com>
2024-07-21 19:26:40 +0700

    styling anime list page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,20 +3,21 @@ import AnimeList from "./components/AnimeList/AnimeList";
 const Home = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`);
   const anime = await res.json();
-  console.log(anime);
 
   return (
     <div>
       <h1>Paling Populer</h1>
-      {anime.data?.map((data) => {
-        return (
-          <AnimeList
-            key={data.mal_id}
-            title={data.title}
-            images={data.images.webp.image_url}
-          />
-        );
-      })}
+      <div className="grid grid-cols-3 gap-4">
+        {anime.data?.map((data) => {
+          return (
+            <AnimeList
+              key={data.mal_id}
+              title={data.title}
+              images={data.images.webp.image_url}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
