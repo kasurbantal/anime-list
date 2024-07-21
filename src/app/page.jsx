8f08dc99b0dfb853bc69ab05d65a3d23f3200198
kasurbantal <kasurbantal880@gmail.com>
2024-07-21 22:19:01 +0700

    adding Navbar component and link to see all popular anime in anime list page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,24 @@
+import Link from "next/link";
 import AnimeList from "./components/AnimeList/AnimeList";
 
 const Home = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
+  );
   const anime = await res.json();
 
   return (
     <div>
-      <h1>Paling Populer</h1>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+      <div className="flex justify-between p-4">
+        <h1 className="font-bold text-2xl">Paling Populer</h1>
+        <Link
+          href="/populer"
+          className="text-xl underline hover:text-indigo-500 transition-all"
+        >
+          Lihat semua
+        </Link>
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
         {anime.data?.map((data) => {
           return (
             <div key={data.mal_id} className="shadow-xl">
