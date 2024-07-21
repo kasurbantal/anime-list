d60a5221049b2e1cf20f01029e42902c2237bc9d
kasurbantal <kasurbantal880@gmail.com>
2024-07-21 21:40:53 +0700

    change grid anime list 4 cols, give id to AnimeList

--- src/app/components/AnimeList/AnimeList.jsx
+++ src/app/components/AnimeList/AnimeList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
-const AnimeList = ({ title, images }) => {
+const AnimeList = ({ title, images, id }) => {
   return (
-    <div className="">
+    <Link href={`${id}`} className="cursor-pointer">
       <Image src={images} alt={title} width={350} height={350} />
       <h3 className="font-bold md:text-xl text-md p-4">{title}</h3>
-    </div>
+    </Link>
   );
 };
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,13 +7,14 @@ const Home = async () => {
   return (
     <div>
       <h1>Paling Populer</h1>
-      <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4">
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
         {anime.data?.map((data) => {
           return (
             <div key={data.mal_id} className="shadow-xl">
               <AnimeList
                 title={data.title}
                 images={data.images.webp.image_url}
+                id={data.mal_id}
               />
             </div>
           );
