d543a52071920d6f8a3d8b2cb413bab313be30b8
kasurbantal <kasurbantal880@gmail.com>
2024-07-23 00:57:24 +0700

    make component AnimeListHeader & AnimeList reusable

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,23 @@
-import Link from "next/link";
 import AnimeList from "../components/AnimeList/AnimeList";
+import AnimeListHeader from "../components/AnimeList/AnimeListHeader";
 
 const Home = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
   );
-  const anime = await res.json();
+  const topAnime = await res.json();
 
   return (
-    <div>
-      <div className="flex justify-between items-center p-4">
-        <h1 className="font-bold text-2xl">Paling Populer</h1>
-        <Link
-          href="/populer"
-          className="md:text-xl text-sm underline hover:text-indigo-500 transition-all"
-        >
-          Lihat semua
-        </Link>
-      </div>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-        {anime.data?.map((data) => {
-          return (
-            <div key={data.mal_id} className="shadow-xl">
-              <AnimeList
-                title={data.title}
-                images={data.images.webp.image_url}
-                id={data.mal_id}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      <section>
+        <AnimeListHeader
+          title="Paling Populer"
+          linkTitle="Lihat semua"
+          linkHref="/popular"
+        />
+        <AnimeList api={topAnime} />
+      </section>
+    </>
   );
 };
 
