3509c24b00c2010fc34e2296023310984f6fb682
kasurbantal <kasurbantal880@gmail.com>
2024-07-25 00:35:04 +0700

    add handleSearch

--- src/components/Navbar/InputSearch.jsx
+++ src/components/Navbar/InputSearch.jsx
@@ -1,14 +1,26 @@
 "use client";
 import { MagnifyingGlass } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
 
 const InputSearch = () => {
+  const searchRef = useRef();
+  const router = useRouter();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const keyword = searchRef.current.value;
+    router.push(`/search/${keyword}`);
+  };
+
   return (
     <div className="relative">
       <input
         placeholder="Looking for anime..."
         className="w-full p-2 rounded"
+        ref={searchRef}
       />
-      <button className="absolute top-2 end-2">
+      <button className="absolute top-2 end-2" onClick={handleSearch}>
         <MagnifyingGlass size={24} />
       </button>
     </div>
