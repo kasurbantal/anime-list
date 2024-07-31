d5d1462977f1d6d8b0f52095620de3b55e8164a4
kasurbantal <kasurbantal880@gmail.com>
2024-07-31 19:18:57 +0700

    feat: add api-libs.js (reusable API)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
 import AnimeList from "../components/AnimeList/AnimeList";
 import AnimeListHeader from "../components/AnimeList/AnimeListHeader";
+import { getAnimeResponse } from "@/app/libs/api-libs";
 
 const Home = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
-  );
-  const topAnime = await res.json();
+  const topAnime = await getAnimeResponse("top/anime", "limit=8");
 
   return (
     <>
