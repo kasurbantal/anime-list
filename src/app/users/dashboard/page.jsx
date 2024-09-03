311cd0a4f259981322a703d94c4a1c8e014839d7
kasurbantal <kasurbantal880@gmail.com>
2024-09-03 20:58:05 +0600

    feat: add bottom in dashboard colletons

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,13 +1,27 @@
 import { authUserSession } from "@/libs/auth-libs";
 import Image from "next/image";
+import Link from "next/link";
 
 const Page = async () => {
   const user = await authUserSession();
   return (
-    <div className="text-color-primary">
-      <h3>DASHBOARD</h3>
-      <h5>Welcome, {user.name} </h5>
+    <div className="mt-8 flex flex-col justify-center items-center text-color-primary">
+      <h5 className="mb-2 text-2xl font-bold">Welcome, {user.name} </h5>
       <Image src={user.image} alt="image" width={250} height={250} />
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link
+          href="/users/dashboard/collections"
+          className="bg-color-secondary text-color-dark font-bold px-4 py-2 text-xl rounded-full transition-all"
+        >
+          My Collections
+        </Link>
+        <Link
+          href="/users/dashboard/comments"
+          className="bg-color-secondary text-color-dark font-bold px-4 py-2 text-xl rounded-full transition-all"
+        >
+          My Comments
+        </Link>
+      </div>
     </div>
   );
 };
